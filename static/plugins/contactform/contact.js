8a042ec1cb4932b8fd92bcaa4fcf0737b477371f
Sean Goheen <106340+Onfire7@users.noreply.github.com>
2020-09-24 18:46:00 -0500

    Update contact.js

--- static/plugins/contactform/contact.js
+++ static/plugins/contactform/contact.js
@@ -11,32 +11,26 @@ form.addEventListener('submit', event => {
   try {
     $crisp.push(["set", "user:nickname", [nameValue]]);
     $("#name").removeClass("form-error");
-    console.log("Name Good");
   }
   catch(err) {
     $("#name").addClass("form-error");
     errcount++;
-    console.log("Name Bad");
   }
   try {
     $crisp.push(["set", "user:email", [emailValue]]);
     $("#email").removeClass("form-error");
-    console.log("Email Good");
   }
   catch(err) {
     $("#email").addClass("form-error");
     errcount++;
-    console.log("Email Bad");
   }
   try {
     $crisp.push(["set", "user:company", [companyValue]]);
     $("#company").removeClass("form-error");
-    console.log("Company Good");
   }
   catch(err) {
     $("#company").addClass("form-error");
     errcount++;
-    console.log("Company Bad");
   }
   
   if(errcount == 0){
@@ -44,13 +38,10 @@ form.addEventListener('submit', event => {
       $crisp.push(["do", "message:send", ["text", messageValue]]);
       $crisp.push(["do", "chat:open"]);
       $("#message").removeClass("form-error");
-      console.log("Message Good");
     }
     catch(err) {
       $("#message").addClass("form-error");
-      console.log("Message Bad");
     }
   }
-  console.log("Errors: ", errcount);
 });
 
