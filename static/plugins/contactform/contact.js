b66c0073398404ded75192ca4b9ff78c4dbb77ae
Sean Goheen <106340+Onfire7@users.noreply.github.com>
2020-09-24 17:31:05 -0500

    Create contact.js

---
+++ static/plugins/contactform/contact.js
@@ -0,0 +1,12 @@
+function contactus(){
+  var emailValue = document.getElementById("email").value;
+  var nameValue = document.getElementById("name").value;
+  var companyValue = document.getElementById("company").value;
+  var messageValue = document.getElementById("message").value;
+  
+  $crisp.push(["set", "user:email", [emailValue]]);
+  $crisp.push(["set", "user:nickname", [nameValue]]);
+  $crisp.push(["set", "user:company", [companyValue]]);
+  $crisp.push(["do", "message:send", ["text", messageValue]])
+  $crisp.push(["do", "chat:open"])
+}
