e529f8121ada96f96ea874c81ebecbb298020bd3
Sean Goheen <106340+Onfire7@users.noreply.github.com>
2020-09-24 18:20:53 -0500

    Update contact.js

--- static/plugins/contactform/contact.js
+++ static/plugins/contactform/contact.js
@@ -2,17 +2,46 @@ const form = document.getElementById("contact-form");
 form.addEventListener('submit', event => {
   event.preventDefault();
   
-  var emailValue = document.getElementById("email").value;
   var nameValue = document.getElementById("name").value;
+  var emailValue = document.getElementById("email").value;
   var companyValue = document.getElementById("company").value;
   var messageValue = document.getElementById("message").value;
   
-  $crisp.push(["set", "user:email", [emailValue]]);
-  $crisp.push(["set", "user:nickname", [nameValue]]);
-  $crisp.push(["set", "user:company", [companyValue]]);
-  $crisp.push(["do", "message:send", ["text", messageValue]]);
-  $crisp.push(["do", "chat:open"]);
+  var errcount = 0;
+  try {
+    $crisp.push(["set", "user:nickname", [nameValue]]);
+    $("name").removeClass("form-error");
+  }
+  catch(Exception e) {
+    $("name").addClass("form-error");
+    errcount++;
+  }
+  try {
+    $crisp.push(["set", "user:email", [emailValue]]);
+    $("email").removeClass("form-error");
+  }
+  catch(Exception e) {
+    $("email").addClass("form-error");
+    errcount++;
+  }
+  try {
+    $crisp.push(["set", "user:company", [companyValue]]);
+    $("company").removeClass("form-error");
+  }
+  catch(Exception e) {
+    $("company").addClass("form-error");
+    errcount++;
+  }
   
-  console.log('Form submission cancelled.');
+  if(errcount == 0){
+    try {
+      $crisp.push(["do", "message:send", ["text", messageValue]]);
+      $crisp.push(["do", "chat:open"]);
+      $("message").removeClass("form-error");
+    }
+    catch(Exception e) {
+      $("message").addClass("form-error");
+    }
+  }
 });
 
